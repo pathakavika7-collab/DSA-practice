8f962a1e7bfc58f8a0b861e200559f854c962e65
pathakavika7-collab <pathakavika7@gmail.com>
2026-06-30 12:31:54 +0530

    Create array_sum.cpp

---
+++ array_sum.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int main() {
+  int arr[]= {1, 2, 3, 4, 5 };
+  int n= 5;
+  int sum=0;
+
+  for(int i=0; i<n: i++) {
+      sum+= arr[i];
+  }
+
+  cout << "sum of array is: "
+    << sum << endl;
+
+   return 0;
+}
